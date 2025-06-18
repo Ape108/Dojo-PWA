f7060203d0cd2d7fc3ea3f943ea871e04c19bd56
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2025-06-18 14:21:21 -0500

    Configure Admin

--- belts/admin.py
+++ belts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Belt, Technique
 
-# Register your models here.
+@admin.register(Belt)
+class BeltAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_order')
+    ordering = ('dispaly_order',)
+
+@admin.register(Technique)
+class TechniqueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'belt', 'order_in_belt')
+    list_filter = ('belt',)
+    search_fields = ('name', 'description')
+    ordering = ('belt__display_order', 'order_in_belt')
+
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Add our custom fields to the admin display
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Dojo Information', {'fields': ('user_type', 'is_approved')}),
+    )
+    list_display = ('username', 'email', 'user_type', 'is_approved', 'is_staff')
+    list_filter = ('user_type', 'is_approved', 'is_staff')
+    # Allow admins to approve users directly from the list view
+    list_editable = ('user_type', 'is_approved')
+    
