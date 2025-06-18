dac9fa4a9ab212048b529db5ed0d02d173455955
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2025-06-18 16:11:54 -0500

    Core Application Views

--- belts/views.py
+++ belts/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+import random
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Belt, Technique
 
-# Create your views here.
+class ApprovedUserRequiredMixin(LoginRequiredMixin):
+    """Custom mixin to verify a user is logged in AND approved by an admin."""
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        # Admins and staff can always access. Students must be approved.
+        if not request.user.is_approved and not request.user.is_staff:
+            return render(request, 'account/pending_approval.html')
+        return super().dispatch(request, *args, **kwargs)
+    
+class BeltDetailView(ApprovedUserRequiredMixin, DetailView):
+    """Displays the main page for a single belt."""
+    model = Belt
+    template_name = 'belts/belt_detail.html'
+    context_object_name = 'belt'
+
+    def get_context_data(self, **kwargs):
+        """Add next/previous belt links to the context for easy navigation."""
+        context = super().get_context_data(**kwargs)
+        all_belts = list(Belt.objects.all())
+        try:
+            current_index = all_belts.index(self.object)
+            context['prev_belt'] = all_belts[current_index - 1] if current_index > 0 else None
+            context['next_belt'] = all_belts[current_index + 1] if current_index < len(all_belts) - 1 else None
+        except ValueError:
+            context['prev_belt'] = None
+            context['next_belt'] = None
+        return context
+    
+class BeltManualView(ApprovedUserRequiredMixin, DetailView):
+    """Displays the embedded PDF training manual for a belt."""
+    model = Belt
+    template_name = 'belts/pdf_viewer.html'
+    context_object_name = 'belt'
+
+class FlashcardSequentialView(ApprovedUserRequiredMixin, ListView):
+    """Displays all techniques for a belt in order (Browse Mode)."""
+    template_name = 'belts/flashcard_list.html'
+    context_object_name = 'techniques'
+
+    def get_queryset(self):
+        self.belt = get_object_or_404(Belt, pk=self.kwargs['belt_pk'])
+        return Technique.objects.filter(belt=self.belt)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['belt'] = self.belt
+        return context
+
+class FlashcardRandomView(ApprovedUserRequiredMixin, DetailView):
+    """Displays one random technique from a belt (Flashcard Mode)."""
+    model = Technique # We are getting a single Technique object
+    template_name = 'belts/flashcard_random.html'
+    context_object_name = 'technique'
+
+    def get_object(self, queryset=None):
+        """Override to select a random technique from the specified belt"""
+        belt_pk = self.kwargs['belt_pk']
+        technique_ids = list(Technique.objects.filter(belt__pk=belt_pk).values_list('id', flat=True))
+        if not technique_ids:
+            return None # Handle case where a belt has no techniques
+        random_id = random.choice(technique_ids)
+        return get_object_or_404(Technique, pk=random_id)
+    
+    def get_context_data(self, **kwargs):
+        """Add the belt to the context for navigation."""
+        context = super().get_context_data(**kwargs)
+        context['belt'] = get_object_or_404(Belt, pk=self.kwargs['belt_pk'])
+        return context
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from belts.models import Belt
 
-# Create your views here.
+class HomeView(ListView):
+    """Displays the list of all belts on the main menu."""
+    model = Belt
+    template_name = 'home.html'
+    context_object_name = 'belts'
+
+# View to serve the manifest.json file
+def manifest_json(request):
+    return render(request, 'manifest.json', content_type='application/json')
+
+# View to serve the service-worker.js file
+def service_worker(request):
+    return render(request, 'service-worker.js', content_type='application/javascript')
+
+
